7580144e32bbcb999a027445c8c18cedb4d33613
ticolo6499 <59658608+ticolo6499@users.noreply.github.com>
2020-09-11 22:29:46 +0700

    ProductDB  load

---
+++ WebApplication1/Controllers/SanPhamController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class SanPhamController : ApiController
+    {
+        //Lấy ds sản phẩm
+        [Route("api/SANPHAM")]
+        [HttpGet]
+        public List<SANPHAM> GET()
+        {
+           SanPhamDBDataContext db = new SanPhamDBDataContext();
+            return db.SANPHAMs.ToList();
+        }
+
+        //Lấy ds sản phẩm theo id sản phẩm
+        [HttpGet]
+        public SANPHAM GetPro(string id)
+        {
+            SanPhamDBDataContext db = new SanPhamDBDataContext();
+            return db.SANPHAMs.FirstOrDefault(x => x.MASP == id);
+        }
+
+        [HttpPost]
+        public bool InsertNewProduct(string name, float cost,string pic, string direc)
+        {
+            try
+            {
+                SanPhamDBDataContext db = new SanPhamDBDataContext();
+                SANPHAM sp = new SANPHAM();
+                sp.TENSP = name;
+                sp.DONGIA = cost;
+                sp.HINHANH = pic;
+                sp.CHITIET = direc;
+                db.SANPHAMs.InsertOnSubmit(sp);
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApplication1/Views/SanPham.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        method: 'GET',
+        url: 'api/SANPHAM',
+        success: function (res) {
+            $.each(res, function (index, item) {
+                var rowHTML = '<tr>'
+                    + '<td>' + item.MASP + '</td>'
+                    + '<td>' + item.TENSP + '</td>'
+                    + '<td>' + item.HINHANH + '</td>'
+                    + '<td>' + item.DONGIA + '</td>'
+                    + '<td>' + item.CHITIET + '</td>'
+                    + '<td>' + item.MASP + '</td>'
+                    + '<td>' + item.MALOAI + '</td>'
+                    + '<td>' + item.MACL + '</td>'
+                    + '<td>' + item.MATH + '</td>'
+                    + '<td>' + item.MATUOI + '</td>'
+                    + '</tr>';
+
+                $('.tbl_sp tbody').append(rowHTML);
+            });
+
+        },
+        error: function (res)
+        {
+            alert("Hệ thống lỗi");
+        }
+    });
+})
